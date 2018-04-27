f782c5806890c6ed6722a2f3837a718ce5e96ab4
mapliu <liudong@aliyun.com>
2018-04-27 15:59:16 +0800

    Initial commit

---
+++ run_lic.bat
@@ -0,0 +1,2 @@
+
+START F:\t32_lic\rlm.v12.1BL3-x64_w3.admin\rlm.exe
---
+++ run_t32.bat
@@ -0,0 +1,5 @@
+
+
+D:
+CD D:\T32_201709\demo\customer\auto_seminar
+START D:\T32_201709\bin\windows64\t32mtc.exe -c F:\auto_seminar_2018\auto_seminar.t32 -s ~~\demo\customer\auto_seminar\main.cmm
